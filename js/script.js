f514086d68c44c2bb3d009b5fea55b8eb61ac501
Yevhenii Slobodianiuk <106830373+Jackie0o@users.noreply.github.com>
2022-11-29 11:48:40 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+"use strict"
+
+console.log ('Я вчу JS')
+
+// ? Основи синтаксису JAVASCRIPT. Структура коду. Інструкції. Режим use strict. Уроки JAVASCRIPT із нуля.
+
+// Варіант № 1
+console.log ('Вчимо JS')
+
+// Варіант № 2
+console.log ('JS')
+console.log ('Вчимо')
+
+// Варіант № 3
+console.log ('Вчимо') console.log ('JS')
+
+// Варіант № 4
+console.log ('Вчимо);
+console.log ('JS');
+
+// ? Змінні у JAVASCRIPT. Константи. Ключові слова LET CONST VAR. Уроки JAVASCRIPT з нуля 2020
+
+let userEyeColor;
+let user;
+let userName;
+userName = 'Вася';
+user = userName;
+console.log(user);
+
+// Варіант № 1
+let user = 'Вася' let age = 36
+
+// Варіант № 2
+let myage = 36;
+
+// Варіант № 3
+const BLOCK_SIZE = 14 + 50;
+
+// Варіант № 4
+const BLOCK_HEIGHT = 100;
+
+// Варіант № 5
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+// ? Типи даних JAVASCRIPT. Null String Undefined Boolean Number та інші. Уроки JAVASCRIPT з нуля 2020
+
+// Варіант № 1
+let userAge = 36;
+let userInfo = "Фрілансер ${userAge}";
+// let userInfo = `Фрілансер ${userAge}`;
+// console.log(userInfo);
+
+// Варіант № 2
+let userHeight = 145 / 0;
+// Повернеться значення NaN
+console.log(userHeight);
+
+// Варіант № 3
+let userName;
+// Повернеться тип даних Null
+console.log(typeof userName);
+
+// Варіант № 4
+let userSize = "45" / "8";
+// let userSize = "Зліва" / "Справа";
+// let userSize = "Зліва45" / "Справа1";
+// console.log(userSize);
+// Повернеться тип даних Number
+console.log(typeof userSize);
